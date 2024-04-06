1f707ee4bd31ba91f2719472ecc46d3a41ec49cf
HARSH SHUKLA <ashuklaharsh2005@gmail.com>
2024-04-06 10:35:27 +0530

    Subs

---
+++ app/Components/assignments.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Assignments () {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- app/Components/mainBody.tsx
+++ app/Components/mainBody.tsx
@@ -2,12 +2,14 @@
 
 import Header from "./header";
 import SmallBlocks from "./smallBlocks";
+import Submissions from "./submissions";
 
 export default function MainBody () {
     return (
         <div className="bg-white min-h-screen h-auto w-screen md:w-[80vw] flex flex-col justify-start items-center py-4">
             <Header />
             <SmallBlocks />
+            <Submissions />
         </div>
     )
 }
